3cc50b6fa182b78249dedeb0047718e1d8ea25cc
Dmitri Gaisan <dgaisan@financialforce.com>
2019-02-27 21:00:04 -0800

    adding result label

--- View Controllers/ResultViewController.swift
+++ View Controllers/ResultViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
     public var isGoing: Bool!
     public var name: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if isGoing {
+            self.label.text = "Thank you \(name!) for attending our event!"
+        } else {
+            self.label.text = "Sorry to hear \(name!) that you're not attending"
+        }
     }
 
     @IBAction func closeView(_ sender: Any) {
